faa6e452c60b323086d800d5124d70db09a1b3c7
mrutyunjayamsrit <mrutyunjayamsrit@gmail.com>
2020-10-01 18:05:41 +0530

    Code optimization

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const socket = require('socket.io');
 const uuid = require('uuid');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const path = require('path');
 const config = require('config');
-const utils = require('./utils/utils');
+const {createSocketConnection, currentUser} = require('./socketio');
 
 const {
-  saveConnectedUsers,
-  saveDisconnectedUsers,
-  saveUserInputMessage,
-  updateUserInputMessage,
-  saveIntentVisited,
-  updateIntentVisited,
   getCompletedUserdeatils,
   getDisconnectedUserDetails,
   getTotalUsersVisitedBot
@@ -24,18 +17,7 @@ const db = require('./database/db');
 const {MongoURL} = config;
 
 // Database functions
-const {findDocuments, insertDocuments, updateDocument, removeDocument} = db;
-let sessionId = '';
-
-//console.log(sessionId);
-
-let userConnected = '';
-
-// flag to insert and update
-let isDataInserted = false;
-let isIntentVisited = false;
-let intents = [];
-let userQuery = [];
+const {removeDocument} = db;
 
 const PORT = process.env.PORT || 3030;
 
@@ -80,82 +62,14 @@ app.use(session({
   saveUninitialized: true
 }));
 
-const io = socket(server);
 
-const activeUsers = new Set();
-
-io.on("connection",(socket)=>{
-    console.log("socket connected successfully");
-
-    // New User event
-    socket.on("new user", function (data) {
-        socket.userId = data;
-        activeUsers.add(data);
-        userConnected = socket.userId;
-        sessionId = uuid.v4();
-        // Store number of users who communicated with bot
-        saveConnectedUsers(userConnected,sessionId);
-        isDataInserted = false;
-        isIntentVisited = false;
-        intents = [];
-        userQuery = [];
-        io.emit("new user", [...activeUsers]);
-      });
-      
-      // diconnect event
-      socket.on("disconnect", () => {
-        console.log('User disconnected: ', socket.userId);
-        saveDisconnectedUsers(socket.userId, sessionId);  
-        activeUsers.delete(socket.userId);
-        io.emit("user disconnected", socket.userId);
-      });
-    
-      // chat message event
-      socket.on("chat message", async function (data) {
-          console.log('Chat Data:', data)
-          io.emit("chat message", data);
-
-          // Store User queries
-          userQuery.push(data.message);
-          if(!isDataInserted){
-            saveUserInputMessage(userConnected, userQuery, sessionId);
-            isDataInserted = true;
-          }else{
-            updateUserInputMessage(userConnected,userQuery);
-          }
-          // get intent deatils and response
-          //const intent = utils.check_intent(data.message);
-          const intentDetails = await utils.getIntentDetailsFromDF(data.message);
-          const [intent, bot_message] = intentDetails;
-          console.log("Intent name: ", intent);
-          intents.push(intent);
-         if(!isIntentVisited){
-           saveIntentVisited(userConnected, intents,sessionId);
-           isIntentVisited = true;
-         }else{
-           updateIntentVisited(userConnected, intents);
-         }
-          // get Bot reply message
-          //const bot_message = utils.getBotMessage(intent); 
-          console.log('Bot Message:', bot_message);
-          data.message = bot_message;
-          data.nick = 'FinBot';
-          io.emit("chat message", data);
-      });
-      
-      socket.on("typing", function (data) {
-        socket.broadcast.emit("typing", data);
-      });
-})
+createSocketConnection(server);
 
 // Api to get current user details
 app.get('/currentUser',(req, res)=>{
   let session = req.session;
   req.session.user = userConnected;
-  res.send({
-    sessionId: sessionId,
-    user: req.session.user
-  })
+  res.send(currentUser);
 });
 
 // API to get total number of users taken chat
@@ -176,6 +90,7 @@ app.get('/disconnectedUser',async (req, res)=>{
   res.send(disconnectedUsers);
 });
 
+// API to delete the existing records from DB
 app.get('/deleteData', async(req,res)=>{
   const collection = req.query.col;
   console.log('collection: ', collection);
---
+++ socketio.js
@@ -0,0 +1,103 @@
+const socket = require('socket.io');
+const uuid = require('uuid');
+const utils = require('./utils/utils')
+
+// flag to insert and update
+let isDataInserted = false;
+let isIntentVisited = false;
+let intents = [];
+let userQuery = [];
+
+let sessionId = '';
+let userConnected = '';
+let currentUser = {};
+
+const {
+  saveConnectedUsers,
+  saveDisconnectedUsers,
+  saveUserInputMessage,
+  updateUserInputMessage,
+  saveIntentVisited,
+  updateIntentVisited,
+  getCompletedUserdeatils,
+  getDisconnectedUserDetails,
+  getTotalUsersVisitedBot
+} = require('./utils/dataStore');
+
+function createSocketConnection(server){
+    const io = socket(server);
+
+    const activeUsers = new Set();
+
+    io.on("connection",(socket)=>{
+        console.log("socket connected successfully");
+
+        // New User event
+        socket.on("new user", function (data) {
+            socket.userId = data;
+            activeUsers.add(data);
+            userConnected = socket.userId;
+            sessionId = uuid.v4();
+            currentUser.user =  userConnected;
+            currentUser.sessionId = sessionId;
+            // Store number of users who communicated with bot
+            saveConnectedUsers(userConnected,sessionId);
+            isDataInserted = false;
+            isIntentVisited = false;
+            intents = [];
+            userQuery = [];
+            io.emit("new user", [...activeUsers]);
+        });
+        
+        // diconnect event
+        socket.on("disconnect", () => {
+            console.log('User disconnected: ', socket.userId);
+            saveDisconnectedUsers(socket.userId, sessionId);  
+            activeUsers.delete(socket.userId);
+            io.emit("user disconnected", socket.userId);
+        });
+        
+        // chat message event
+        socket.on("chat message", async function (data) {
+            console.log('Chat Data:', data)
+            io.emit("chat message", data);
+
+            // Store User queries
+            userQuery.push(data.message);
+            if(!isDataInserted){
+                saveUserInputMessage(userConnected, userQuery, sessionId);
+                isDataInserted = true;
+            }else{
+                updateUserInputMessage(userConnected,userQuery);
+            }
+            // get intent deatils and response
+            //const intent = utils.check_intent(data.message);
+            const intentDetails = await utils.getIntentDetailsFromDF(data.message);
+            const [intent, bot_message] = intentDetails;
+            console.log("Intent name: ", intent);
+            intents.push(intent);
+            if(!isIntentVisited){
+            saveIntentVisited(userConnected, intents,sessionId);
+            isIntentVisited = true;
+            }else{
+            updateIntentVisited(userConnected, intents);
+            }
+            // get Bot reply message
+            //const bot_message = utils.getBotMessage(intent); 
+            console.log('Bot Message:', bot_message);
+            data.message = bot_message;
+            data.nick = 'FinBot';
+            io.emit("chat message", data);
+        });
+        
+        socket.on("typing", function (data) {
+            socket.broadcast.emit("typing", data);
+        });
+    });
+}
+
+
+module.exports = {
+    createSocketConnection,
+    currentUser
+}
